086e3d1e7aaee4eda9483ba61e12e55827518130
tsaniarisqi <tsaniarisqi98@gmail.com>
2022-09-13 22:04:40 +0700

    menambahkan include

---
+++ include/sidebar.php
@@ -0,0 +1,32 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+	<!-- Sidebar - Brand -->
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+		<div class="sidebar-brand-icon rotate-n-15">
+			<i class="fas  fa-envelope"></i>
+		</div>
+		<div class="sidebar-brand-text mx-3">Arsip Surat</div>
+	</a>
+
+	<!-- Divider -->
+	<hr class="sidebar-divider">
+
+	<!-- Heading -->
+	<div class="sidebar-heading">
+		Menu
+	</div>
+
+	<!-- Nav Item - Arsip -->
+	<li class="nav-item active">
+		<a class="nav-link" href="arsip.html">
+			<i class="fas fa-fw fa-star"></i>
+			<span>Arsip</span></a>
+	</li>
+
+	<!-- Nav Item - About -->
+	<li class="nav-item active">
+		<a class="nav-link" href="about.html">
+			<i class="fas fa-fw fa-info-circle "></i>
+			<span>About</span></a>
+	</li>
+</ul>
